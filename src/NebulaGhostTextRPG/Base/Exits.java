09e0b0885c462178b2380b54138f373e8028f647
HuntorWorks <rt001a9138@gmail.com>
2026-02-03 13:28:34 +0000

    FEATURE: added properties to Exit enum to pull directly from the enum for the navigation list in DialogueManager.java

--- src/NebulaGhostTextRPG/Base/Exits.java
+++ src/NebulaGhostTextRPG/Base/Exits.java
@@ -1,8 +1,19 @@
 package NebulaGhostTextRPG.Base;
 
 public enum Exits {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
+    NORTH("[N]orth "),
+    EAST("[E]ast "),
+    SOUTH("[S]outh "),
+    WEST("[W]est ");
+
+    private final String displayName;
+
+    Exits(String displayName) {
+       this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
+
 }
--- src/NebulaGhostTextRPG/Managers/DialogueManager.java
+++ src/NebulaGhostTextRPG/Managers/DialogueManager.java
@@ -12,4 +12,15 @@ public class DialogueManager {
     public void showRoomDialogue(Room room) {
         System.out.println(room.getRoomName());
     }
+
+    public void showRoomNavigation(Room room) {
+        StringBuilder navSb = new StringBuilder();
+        var exits = room.getExits();
+
+        navSb.setLength(0);
+        for (var exit : exits) {
+            navSb.append(exit.getDisplayName());
+        }
+        System.out.println(navSb);
+    }
 }
