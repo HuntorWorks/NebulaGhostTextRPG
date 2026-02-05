e2f721a1e21e845932e11f07c284b3bb1d2a9a87
HuntorWorks <rt001a9138@gmail.com>
2026-02-05 19:30:41 +0000

    FEATURE: Added first RoomObject

--- src/NebulaGhostTextRPG/Rooms/PodStorage.java
+++ src/NebulaGhostTextRPG/Rooms/PodStorage.java
@@ -2,6 +2,7 @@ package NebulaGhostTextRPG.Rooms;
 
 import NebulaGhostTextRPG.Base.*;
 import NebulaGhostTextRPG.Data.StoryRegistry;
+import NebulaGhostTextRPG.Rooms.RoomObjects.WallTerminal;
 
 public class PodStorage extends Room {
 
@@ -9,5 +10,17 @@ public class PodStorage extends Room {
         super(roomState, exits);
         this.description = StoryRegistry.POD_STORAGE_DESC;
         this.roomName = "Cryo-Pod Storage";
+        generateRoomObjects();
     }
+
+    private void generateRoomObjects() {
+        RoomObject wallTerminal = new WallTerminal(
+                "Wall Terminal",
+                "{TEMP DESC}",
+                InteractionType.INVESTIGATE,
+                InteractionType.HACK
+        );
+        this.roomObjects.add(wallTerminal);
+    }
+
 }
--- src/NebulaGhostTextRPG/Rooms/RoomObjects/WallTerminal.java
+++ src/NebulaGhostTextRPG/Rooms/RoomObjects/WallTerminal.java
@@ -1,4 +1,27 @@
 package NebulaGhostTextRPG.Rooms.RoomObjects;
 
-public class WallTerminal {
+import NebulaGhostTextRPG.Base.InteractionType;
+import NebulaGhostTextRPG.Base.RoomObject;
+
+public class WallTerminal extends RoomObject {
+    public WallTerminal(String objectName, String objDescription, InteractionType... availableInteractions) {
+        super(objectName, objDescription, availableInteractions);
+    }
+
+    @Override
+    public void handleInteraction(InteractionType interactionType) {
+        switch (interactionType) {
+            case HACK: {
+                if (!this.hasInvestigated()) {
+                    return;
+                }
+                System.out.println("You Hacked The Terminal");
+            }
+            case INVESTIGATE: {
+                System.out.println("You investigated the Terminal.");
+            }
+            default:
+                super.handleInteraction(interactionType);
+        }
+    }
 }
